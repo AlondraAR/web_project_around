fdad455dad2f5ca440f0713d170574dff2bfed2d
Alondra Arriaga <aloshreyes@gmail.com>
2025-02-19 11:10:43 -0600

    Se agrega popup image

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,13 @@ let addButton = content.querySelector('.form__button-save')
 
 // Seleccionar elementos del DOM
 const editButton = document.querySelector('.content-profile__info-editButtom');
-const formContainer = document.querySelector('.form');
+const formProfileContainer = document.querySelector('.formProfile');
 const closeButton = document.querySelector('.form__button-close');
+const closeButtonPhoto = document.querySelector('.form__button-closePhoto');
+const addPhotoBottom = document.querySelector('.content-profile__add-buttom');
+const formPhotoContainer = document.querySelector('.formPhoto');
+const createPhotoBotton = document.querySelector('.form__button-savePhoto');
+
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -32,6 +37,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
   }
 ];
+
 const photoContainer = document.querySelector('.content-photos');
 
 initialCards.forEach(function addInitialPhoto(item) {
@@ -41,6 +47,8 @@ initialCards.forEach(function addInitialPhoto(item) {
   
     photoElement.querySelector(".content-photos__description").textContent = item.name;
     photoElement.querySelector(".content-photos__photo").src = item.link;
+    photoElement.querySelector(".content-photos__photo").alt = item.name;
+
   console.log(item.name);
   console.log(item.link);
     photoElement.querySelector('.content-photos__button-like').addEventListener("click", function (evt) {
@@ -55,12 +63,20 @@ initialCards.forEach(function addInitialPhoto(item) {
 // Función para mostrar el formulario
 editButton.addEventListener('click', () => {
     console.log("OK clickado");
-    formContainer.style.display = 'block'; // Mostrar el formulario
+    formProfileContainer.style.display = 'block'; // Mostrar el formulario
 });
 
 // Función para cerrar el formulario
 closeButton.addEventListener('click', () => {
-    formContainer.style.display = 'none'; // Ocultar el formulario
+    formProfileContainer.style.display = 'none'; // Ocultar el formulario
+});
+
+addPhotoBottom.addEventListener('click', () => {
+  console.log("OK clickado");
+  formPhotoContainer.style.display = 'block'; // Mostrar el formulario
+});
+closeButtonPhoto.addEventListener('click', () => {
+  formPhotoContainer.style.display = 'none'; // Ocultar el formulario
 });
 
 
@@ -94,22 +110,56 @@ window.onload = function() {
 
       addButton.addEventListener('click', (event) => {
         submitNewInformation(event);
-        formContainer.style.display = 'none';
+        formProfileContainer.style.display = 'none';
     });
 
-    function addPhoto(photoTitleValue, photoUrlValue) {
+    function addPhoto(event) {
+      event.preventDefault();
+
+      const photoTitleValue = document.getElementById('title').value;
+      const photoUrlValue = document.getElementById('link').value;
+
       const photoTemplate = document.querySelector("#photo-template").content;
       const photoElement = photoTemplate.querySelector('.content-photos__photo-description').cloneNode(true);
     
       photoElement.querySelector(".content-photos__description").textContent = photoTitleValue;
-      photoElement.querySelector(".content-photos__photo").textContent = photoUrlValue;
+      photoElement.querySelector(".content-photos__photo").src = photoUrlValue;
     
       photoElement.querySelector('.content-photos__button-like').addEventListener("click", function (evt) {
         evt.target.classList.toggle("content-photos__button-like_active");
         });
-      
-      photoContainer.append(photoElement);
-      
+
+      photoContainer.prepend(photoElement);
+      photoContainer.removeChild(photoContainer.lastElementChild);
     }
 
+    createPhotoBotton.addEventListener('click', (event) => {
+      addPhoto(event);
+      formPhotoContainer.style.display = 'none';
+  });
 
+  const deleteButtons = document.querySelectorAll('.content-photos__deleteButton');
+  deleteButtons.forEach(function(boton){
+    boton.addEventListener('click',function() {
+      console.log('ok click');
+      const deleteElement = boton.parentElement;
+      console.log(deleteElement);
+      deleteElement.remove();
+      }
+
+    )
+  });
+  
+  document.querySelectorAll('.content-photos img').forEach(image => {
+   image.onclick = () => {
+    console.log('Imagen clickeada')
+   document.querySelector('.popup-image').style.display = 'block';
+   document.querySelector('.popupimage').src = image.getAttribute('src');
+   document.querySelector('.popup_description').textContent = image.getAttribute('alt');
+   console.log(image.getAttribute('alt'));
+   }
+    });
+    
+    document.querySelector('.popup-image span').onclick = () => { //funcion para cuando se de clic en la "x"
+     document.querySelector('.popup-image').style.display = 'none'; //ocultamos el contenedor emergente
+    }
